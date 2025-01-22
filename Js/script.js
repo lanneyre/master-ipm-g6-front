c6169ac52ac40010b8037f8afa40885593ee2689
Wolf <r.lanney@ics-nice.com>
2025-01-22 09:30:17 +0100

    contact

--- Js/script.js
+++ Js/script.js
@@ -25,12 +25,27 @@ function horloge() {
 
 //fonction qui permet de créer une boîte de dialogue plus sympatique que celle de js
 function modal(titre, message) {
-    if (titre && message) {
+    // console.log(typeof (message) == "object");
+    document.querySelector("#modal>#content>div").classList.remove("products")
+    document.querySelector("#modal>#content>div").setAttribute("id", "")
+    if (titre && typeof (message) == "object") {
+        //console.log("test");
+        document.querySelector("#modal>#content>h3").innerText = titre;
+        document.querySelector("#modal>#content>div").innerHTML = ""
+        document.querySelector("#modal>#content>div").setAttribute("id", "produits")
+        document.querySelector("#modal>#content>div").classList.add("products")
+        document.querySelector("#modal>#content>div").appendChild(message)
+    }
+    else if (titre && message) {
         document.querySelector("#modal>#content>h3").innerText = titre;
         document.querySelector("#modal>#content>div").innerHTML = message;
 
-        document.querySelector("#modal").classList.toggle("active");
+
     }
+    if (!document.querySelector("#modal").classList.contains("active")) {
+        document.querySelector("#modal").classList.add("active");
+    }
+
 }
 
 let produits = [
@@ -48,7 +63,15 @@ let produits = [
     { titre: "Excursion culturelle sur Vénus", image: "Venus.png", desc: "Découvrez les bases humaines installées dans les nuages de Vénus et apprenez l’histoire fascinante de la conquête de cette planète inhospitalière.", prix: 19000000.00, dateDepart: new Date(2342, 11, 29, 12, 0, 0) }
 ]
 
-function createProduct(produit) {
+function filtrerProduitsParDate(dateCible) {
+    return produits.filter(produit => produit.dateDepart.getTime() === dateCible.getTime());
+}
+
+
+function createProduct(produit, btn) {
+    if (typeof (btn) != "boolean") {
+        btn = true
+    }
     let sec = document.createElement("section")
     sec.classList.add("produit")
 
@@ -75,11 +98,13 @@ function createProduct(produit) {
     p.innerHTML = "Départ le " + (produit.dateDepart).toLocaleString()
     div.appendChild(p);
 
+    if (btn) {
+        let btn = document.createElement("button")
+        btn.classList.add("btn")
+        btn.innerHTML = "Réservez maintenant";
+        asi.appendChild(btn);
+    }
 
-    let btn = document.createElement("button")
-    btn.classList.add("btn")
-    btn.innerHTML = "Réservez maintenant";
-    asi.appendChild(btn);
 
     let art = document.createElement("article");
     art.innerHTML = produit.desc
@@ -88,17 +113,162 @@ function createProduct(produit) {
     return sec;
 }
 
+function generateCalendar(d) {
+
+    if (!d) {
+        d = new Date();
+    }
+    // je créais une variable tableau
+    var table = document.createElement('table');
+    table.className = "cal_calendrier";
+    // je vais chercher dans l'élément calendrier dans le dom et j'attache l'enfant table
+    document.getElementById('calendrier').innerHTML = '';
+    document.getElementById('calendrier').appendChild(table);
+    // first line 
+    var tr = document.createElement('tr');
+    table.appendChild(tr);
+
+    var th = document.createElement('th');
+    th.classList.add("navigation")
+    // on fusionne 2 cellules
+    th.setAttribute('colspan', '2');
+
+    // on attribue un boutton avec un evenement
+    var button = document.createElement("button");
+    button.innerHTML = "<<";
+    button.setAttribute("id", "moinsUnAn");
+    th.appendChild(button);
+
+    //l'evenement
+    button.addEventListener("click", function () {
+        generateCalendar(new Date(d.getFullYear() - 1, d.getMonth(), d.getDate()));
+    });
+    // on attribue un boutton avec un evenement
+    button = document.createElement("button");
+    button.innerHTML = "<";
+    button.setAttribute("id", "moinsUnMoi");
+    button.addEventListener("click", function () {
+        generateCalendar(new Date(d.getFullYear(), d.getMonth() - 1, d.getDate()));
+    });
+    th.appendChild(button);
+
+    tr.appendChild(th);
+
+    th = document.createElement('th');
+    th.setAttribute('colspan', '3');
+    th.classList.add("navigation")
+    tr.appendChild(th);
+
+    // on a besoin d'une date
+
+    // on a besoin d'une variable mois contenant le texte
+    var month = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+    // on met ce qu'on veut
+    th.innerHTML = month[d.getMonth()] + ' ' + d.getFullYear();
+    // on met ce qu'on veut mais le HTML ne sera pas interprété
+    // th.textContent = 
+    // version la plus propre, cohérence dans la méthode
+    // th.appendChild(document.createTextNode(""));
+
+    th = document.createElement('th');
+    th.classList.add("navigation")
+    th.setAttribute('colspan', '2');
+    // on attribue un boutton avec un evenement
+    button = document.createElement("button");
+    button.innerHTML = ">";
+    button.setAttribute("id", "plusUnMois");
+    button.addEventListener("click", function () {
+        generateCalendar(new Date(d.getFullYear(), d.getMonth() + 1, d.getDate()));
+    });
+    th.appendChild(button);
+    button = document.createElement("button");
+    button.innerHTML = ">>";
+    button.setAttribute("id", "plusUnAn");
+
+    button.addEventListener("click", function () {
+        generateCalendar(new Date(d.getFullYear() + 1, d.getMonth(), d.getDate()));
+    });
+
+    th.appendChild(button);
+
+    tr.appendChild(th);
+
+    var day = ['dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam'];
+    tr = document.createElement('tr');
+    table.appendChild(tr);
+
+
+    for (i = 0; i < day.length; i++) {
+        th = document.createElement("th");
+        th.innerHTML = day[i];
+        tr.appendChild(th);
+    }
+
+    function daysInMonth(year, month) {
+        return new Date(year, month + 1, 0).getDate();
+    }
+
+    var firstDay = new Date(d.getFullYear(), d.getMonth(), 1).getDay();
+
+    var numero = 1;
+    // variable permettant d'afficher les jours du mois suivant
+    var nextMonth = 1;
+    // variable permettant d'afficher les jours du mois précédent
+    var previousMonth = daysInMonth(d.getFullYear(), d.getMonth() - 1) - firstDay + 1;
+
+    for (var i = 0; i < 6; i++) {
+        tr = document.createElement('tr');
+        table.appendChild(tr);
+        for (var j = 0; j < 7; j++) {
+            var td = document.createElement('td');
+            tr.appendChild(td);
+            // console.log(i);
+            if (i == 0 && j < firstDay) {
+                td.innerHTML = (previousMonth++).toLocaleString("fr", { minimumIntegerDigits: 2, useGrouping: false });
+                td.className = "cal_jours_av_ap";
+            } else if (numero <= daysInMonth(d.getFullYear(), d.getMonth())) {
+                td.innerHTML = numero.toLocaleString("fr", { minimumIntegerDigits: 2, useGrouping: false });
+                // Si j'arrive au dernier jour je ne rajoute pas d'autres lignes 
+                if (numero == daysInMonth(d.getFullYear(), d.getMonth())) {
+                    i = 6;
+                }
+                // alert(new Date(d.getFullYear(), d.getMonth(), d.getDate()));
+                // si aujourdhui tombe a ce moment il faut le mettre en valeur
+                var aujourdhui = new Date();
+                if (aujourdhui.getFullYear() == d.getFullYear() && aujourdhui.getMonth() == d.getMonth() && aujourdhui.getDate() == numero) {
+                    td.className = "cal_aujourdhui";
+                }
+                let dateCible = new Date(d.getFullYear(), d.getMonth(), numero, 12, 0, 0);
+                let sejour = filtrerProduitsParDate(dateCible);
+                if (sejour.length == 1) {
+                    //on rajoute l'event
+                    const s = sejour[0]
+                    let img = document.createElement("img");
+                    img.setAttribute("src", "Images/src/" + s.image);
+                    img.setAttribute("alt", s.titre);
+                    img.classList.add("depart")
+
+                    img.addEventListener("click", () => {
+                        modal(s.titre, createProduct(s, false))
+                    })
+                    td.appendChild(img)
+                    //console.log(s)
+                }
+                numero++;
+            } else {
+                // je rajoute les numéros du mois suivant si besoin
+                td.innerHTML = (nextMonth++).toLocaleString("fr", { minimumIntegerDigits: 2, useGrouping: false });
+                td.className = "cal_jours_av_ap";
+                i = 6;
+            }
+        }
+    }
+}
+
 
 window.addEventListener("load", () => {
-    //temps réel 
-    //lance CompteARebours toutes les secondes 
-    if (document.querySelector("#horloge") != null) {
-        setInterval(compteARebours, 1000);
-    }
-    //lance horloge toutes les secondes
-    if (document.querySelector("#comptearebour") != null) {
-        setInterval(horloge, 1000);
-    }
+
+
 
     let products = document.querySelector("#produits");
     if (products != null) {
@@ -133,7 +303,7 @@ window.addEventListener("load", () => {
         document.querySelector("#modal").classList.remove("active");
     });
 
-
+    // Gestion du carrousel
     if (document.querySelector('.carousel') != null) {
         const track = document.querySelector('.carousel-track');
         const slides = Array.from(track.children);
@@ -183,4 +353,88 @@ window.addEventListener("load", () => {
         // Start auto-play on load
         startAutoPlay();
     }
+
+    //gestion du calendrier
+    if (document.querySelector('#calendrier') != null) {
+        generateCalendar()
+    }
+
+    //temps réel
+    //lance CompteARebours toutes les secondes 
+    if (document.querySelector("#horloge") != null) {
+        setInterval(horloge, 1000);
+    }
+    //lance horloge toutes les secondes
+    if (document.querySelector("#comptearebour") != null) {
+        setInterval(compteARebours, 1000);
+    }
+
+    //pour charger les dates du calendrier 
+    if (document.querySelector("a.jourj") != null) {
+        document.querySelector("a.jourj").addEventListener("click", () => {
+            generateCalendar(new Date(2342, 10, 4))
+        })
+    }
+
+    if (document.querySelector("a.now") != null) {
+        document.querySelector("a.now").addEventListener("click", () => {
+            generateCalendar(new Date())
+        })
+    }
+
+    if (document.querySelector("#editor")) {
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+    }
+
+    document.getElementById('contactForm').addEventListener('submit', function (e) {
+        e.preventDefault(); // Empêche l'envoi du formulaire
+
+        // Réinitialise les messages d'erreur
+        document.getElementById('nameError').textContent = "";
+        document.getElementById('emailError').textContent = "";
+        document.getElementById('messageError').textContent = "";
+
+        // Récupère les valeurs des champs
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const message = quill.getText(); //document.getElementById('editor').innerHTML.trim();
+
+
+        let isValid = true;
+
+        // Validation du nom
+        if (name === "") {
+            document.getElementById('nameError').textContent = "Le nom est obligatoire.";
+            isValid = false;
+        }
+
+        // Validation de l'e-mail
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (email === "") {
+            document.getElementById('emailError').textContent = "L'e-mail est obligatoire.";
+            isValid = false;
+        } else if (!emailPattern.test(email)) {
+            document.getElementById('emailError').textContent = "L'e-mail n'est pas valide.";
+            isValid = false;
+        }
+
+        // Validation du message
+        console.log(message.length);
+
+        if (message.length < 15 || message === "<br>") {
+            document.getElementById('messageError').textContent = "Le message doit contenir au moins 15 caractères.";
+            isValid = false;
+        }
+
+        // Si tout est valide, afficher un message ou envoyer le formulaire
+        if (isValid) {
+
+
+            const enrichedMessage = `<strong>Nom :</strong> ${name}<br><strong>E-mail :</strong> ${email}<br><strong>Votre message :</strong> ${quill.getSemanticHTML()}`;
+            modal("Votre message a bien été envoyé !", enrichedMessage)
+        }
+    });
+
 })
